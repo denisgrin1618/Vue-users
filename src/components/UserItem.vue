f115065084b4fb8f6afbb731df0072e4d2dbdcfb
denis <denis.grin1618@gmail.com>
2021-06-06 13:42:12 +0300

    same size item

--- src/components/UserItem.vue
+++ src/components/UserItem.vue
@@ -1,48 +1,55 @@
 <template>
-  <div class="bg m-2 flex flex-col gap-x-0 gap-y-0 border-4 border-pur px-0  max-w-md mx-auto rounded-3xl shadow-lg ">
+  <div class="bg m-2 grid grid-cols-3 gap-x-0 gap-y-0 border-4 border-pur px-0  max-w-md mx-auto rounded-3xl shadow-lg ">
     
-    <div  class="col-span-2 border-b-4 border-pur h-14 py-2 ">
+    <div  class="col-span-3 border-b-4 border-pur h-14 py-2 ">
       <p class="text-3xl text-pur font-black">
           IDENTIFICATION CARD
         </p>
     </div>
-    <div class="flex flex-row px-2 py-2" >
+    <!-- <div class="flex flex-row px-2 py-2" > -->
 
-      <div class="">
+      <div class="p-3">
         <img class="border-4 border-pur h-40 rounded bg-blue-300 sm:mx-0 " 
             :src="user.avatar"
             alt="Woman's Face">
-          <p class="text-2xl text-pur font-cookie">
+          <div class="text-2xl text-pur font-cookie whitespace-nowrap">
           {{user.last_name}} {{user.first_name}}
-          </p>
+          </div>
       </div>
       
-      <div class="flex flex-col text-left  sm:text-left pl-5">
+      <div class="flex flex-col col-span-2 text-left  sm:text-left pt-3">
         
-        <p class="text-lg text-xl text-pur font-semibold" >
-          ID {{user.social_insurance_number}}
-        </p>
-        <p class="text-pur mt-2 font-normal font-semibold" >
-          email: {{user.email}}
-        </p>
-        <p class="text-pur -mt-2 font-normal font-semibold">
-          dob: {{user.date_of_birth}}
-        </p>
-        <p class="text-pur -mt-2 font-normal font-semibold">
-          phone: {{user.phone_number}}
-        </p>
-        <p class="text-pur -mt-2 font-normal font-semibold">
-          gender: {{user.gender}}
-        </p>
         
-        <div class="flex flex-grow items-end ">
+          <p class="text-lg text-xl text-pur font-semibold" >
+            ID {{user.social_insurance_number}}
+          </p>
+       
+
+<div class="grid grid-cols-5">
+        
+          <p class="text-pur mt-2 font-normal font-semibold">email:</p>
+          <p class="text-pur mt-2 font-normal font-semibold col-span-4 pl-2">{{user.email}}</p>
+       
+          <p class="text-pur -mt-2 font-normal font-semibold">dob:</p>
+          <p class="text-pur -mt-2 font-normal font-semibold col-span-4 pl-2">{{user.date_of_birth}}</p>
+       
+          <p class="text-pur -mt-2 font-normal font-semibold">phone:</p>
+          <p class="text-pur -mt-2 font-normal font-semibold col-span-4 pl-2">{{user.phone_number}}</p>
+          
+          <p class="text-pur -mt-2 font-normal font-semibold">gender:</p>
+          <p class="text-pur -mt-2 font-normal font-semibold col-span-4 pl-2">{{user.gender}}</p>
+
+ </div>
+        
+        
+        <div class="flex flex-grow items-end pb-3">
           <p class="text-lg  text-pur font-semibold mt-3 ">
             {{user.address.street_address}}, {{user.address.city}}, {{user.address.zip_code}}
           </p>
         </div>
       </div>
 
-    </div>
+    <!-- </div> -->
 
   </div>
 </template>
